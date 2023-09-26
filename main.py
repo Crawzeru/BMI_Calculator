53b952d0566f176a68601dc6a467cbf3ab238336
Bahadır Birinci <Eudorina67@hotmail.com>
2023-09-26 22:19:08 +0300

    Second release -error fix

--- main.py
+++ main.py
@@ -4,30 +4,38 @@ from tkinter import *
 window =Tk()
 window.title("BMI calculator")
 window.minsize(200,200)
-
-
 def calculate_button():
     global result_label
-    weight=float(weight_entry.get())
-    height=float(height_entry.get())
-    bmi=round(weight/((height/100)*(height/100)),2)
-    if bmi<=18.4:
-        condution="Underweight"
-    elif bmi >18.4 and bmi <24.9:
-        condution="Normal"
-    elif bmi >24.9 and bmi <40:
-        condution="Overweight"
+    weight=(weight_entry.get())
+    height=(height_entry.get())
+    if weight=="" or height == "":
+        result_label.config(text="Enter both weight and height")
     else:
-        condution="Obese"
-    result_label.config(text=f"Your BMI is:{bmi}.You are {condution}. ")
+        try:
+            bmi=float(weight)/((float(height)/100)**2)
+            result_bmi=round(bmi,2)
+            if bmi<=18.4:
+                condution="Underweight"
+            elif bmi >18.4 and bmi <24.9:
+                condution="Normal"
+            elif bmi >24.9 and bmi <40:
+                condution="Overweight"
+            else:
+                condution="Obese"
+            result_label.config(text=f"Your BMI is:{result_bmi}.You are {condution}. ")
+        except:
+            result_label.config(text="Enter valid number!")
 
 
 #label-Weight
+
 weight_label=Label(text="Enter your weight(kg)")
 weight_label.pack()
+
 #entry-weight
 weight_entry=Entry()
 weight_entry.pack()
+
 #label-Height
 height_label=Label(text="Enter your height(cm)")
 height_label.pack()
@@ -40,5 +48,6 @@ calculate_button.pack()
 #result-label
 result_label = Label(text=" ")
 result_label.pack(side=BOTTOM)
+
 
 window.mainloop()
