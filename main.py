25bd4f589a13bcf156ab054f6b9e2ed5fe2c81ce
Bahadır Birinci <Eudorina67@hotmail.com>
2023-09-26 21:16:41 +0300

    First Release

---
+++ main.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+#window
+window =Tk()
+window.title("BMI calculator")
+window.minsize(200,200)
+
+
+def calculate_button():
+    global result_label
+    weight=float(weight_entry.get())
+    height=float(height_entry.get())
+    bmi=round(weight/((height/100)*(height/100)),2)
+    if bmi<=18.4:
+        condution="Underweight"
+    elif bmi >18.4 and bmi <24.9:
+        condution="Normal"
+    elif bmi >24.9 and bmi <40:
+        condution="Overweight"
+    else:
+        condution="Obese"
+    result_label.config(text=f"Your BMI is:{bmi}.You are {condution}. ")
+
+
+#label-Weight
+weight_label=Label(text="Enter your weight(kg)")
+weight_label.pack()
+#entry-weight
+weight_entry=Entry()
+weight_entry.pack()
+#label-Height
+height_label=Label(text="Enter your height(cm)")
+height_label.pack()
+#entry-height
+height_entry=Entry()
+height_entry.pack()
+#button
+calculate_button=Button(text="Calculate",command=calculate_button)
+calculate_button.pack()
+#result-label
+result_label = Label(text=" ")
+result_label.pack(side=BOTTOM)
+
+window.mainloop()
